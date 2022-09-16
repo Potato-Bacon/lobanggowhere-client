c8ae4077012c7b80c7461e2b982e97a93b35ee12
potatobacon <andylohbiz@gmail.com>
2022-09-16 17:42:05 +0800

    created HTML form for submission

--- src/pages/Submission.jsx
+++ src/pages/Submission.jsx
@@ -4,6 +4,69 @@ function Submission() {
   return (
     <>
       <h1>Submission</h1>
+      <form>
+        <fieldset>
+          <legend>Submission</legend>
+          <input type="file" />
+          <br />
+          <label>
+            Title
+            <input name="title" />
+          </label>
+          <br />
+          <label>
+            Description
+            <textarea name="description" rows="6" cols="60" />
+          </label>
+          <br />
+          <label>
+            Vendor
+            <input name="Vendor" />
+          </label>
+          <br />
+          <label>
+            In-Store
+            <input type="checkbox" name="in-store" />
+          </label>
+          <label>
+            Online
+            <input type="checkbox" name="online" />
+          </label>
+          <br />
+          <label>
+            Link to source
+            <input name="url" />
+          </label>
+          <br />
+          <label>
+            {" "}
+            Address
+            <input name="address" />
+          </label>
+          <label>
+            Free
+            <input type="radio" name="dealscategory" value="free" />
+          </label>
+          <label>
+            Price Discounts
+            <input type="radio" name="dealscategory" value="discounts" />
+          </label>
+          <label>
+            Custom
+            <input type="radio" name="dealscategory" value="custom" />
+          </label>
+          <br />
+          <label>
+            Start Date
+            <input type="date" name="startdate" />
+          </label>
+          <label>
+            End Date
+            <input type="date" name="enddate" />
+          </label>
+        </fieldset>
+        <button>Submit</button>
+      </form>
     </>
   );
 }
