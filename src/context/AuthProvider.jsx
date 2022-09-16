bc7c7949e6b856f6f4b67aa7cc33c3f42083aa82
Ernest-Low <ernestlow32@gmail.com>
2022-09-16 16:46:16 +0800

    made jake a string zzz

--- src/context/AuthProvider.jsx
+++ src/context/AuthProvider.jsx
@@ -4,7 +4,7 @@ const AuthContext = createContext({});
 
 export const AuthProvider = ({ children }) => {
   const [auth, setAuth] = useState({});
-  const [user, setUser] = useState({ userName: Jake, password: 123 });
+  const [user, setUser] = useState({ userName: "Jake", password: 123 });
 
   return (
     <AuthContext.Provider value={{ auth, setAuth, user, setUser }}>
