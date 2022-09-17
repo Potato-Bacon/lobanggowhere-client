b40f59ec217a52bc72ba07ab0261f737e6165eea
potatobacon <andylohbiz@gmail.com>
2022-09-18 04:11:29 +0800

    submission form post success

--- src/pages/Submission.jsx
+++ src/pages/Submission.jsx
@@ -1,7 +1,21 @@
 import { Field, Formik } from "formik";
+import { useEffect, useState } from "react";
 import * as Yup from "yup";
+import { SERVER } from "../utils/constants";
+import axios from "axios";
 
 function Submission() {
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    const fetchCategories = async () => {
+      const url = SERVER + "/category";
+      const data = await axios.get(url);
+      setCategories(data);
+    };
+    fetchCategories();
+  }, []);
+
   const today = new Date(Date.now());
   return (
     <>
@@ -10,38 +24,52 @@ function Submission() {
         initialValues={{
           img: "",
           title: "",
+          category: "",
           description: "",
           vendor: "",
           onlineAndOrStore: "",
           url: "",
           address: "",
-          dealsCategory: false,
+          dealsCategory: "",
           priceBeforeDiscount: "",
           priceAfterDiscount: "",
           custom: "",
           startDate: "",
+          submittedBy: "Andy",
           endDate: "",
         }}
-        onSubmit={(values) => {
+        onSubmit={async (values) => {
+          const url = SERVER + "/submission";
           console.log(values);
+          try {
+            await axios.post(url, values);
+          } catch (error) {
+            error.response.data.msg;
+          }
         }}
-
-        // validationSchema={Yup.object({
-        //   img: Yup.mixed().required("File is required"),
-        //   title: Yup.string()
-        //     .max(20, "Must be 30 characters or less")
-        //     .min(8, "Must be 8 characters and more")
-        //     .required("Title is required"),
-        //   description: Yup.string().required("Description is required"),
-        //   vendor: Yup.string().required("Vendor is required"),
-        //   onlineAndOrStore: Yup.boolean().oneOf([true], "Select at least one"),
-        //   url: Yup.string().required("Link is required").url("Link is invalid"),
-        //   dealsCategory: Yup.boolean().oneOf([true], "Select at least one"),
-        //   priceBeforeDiscount: Yup.number().required("Price is required"),
-        //   priceAfterDiscount: Yup.number().required("Price is required"),
-        //   custom: Yup.string().required("Field is required"),
-        //   endDate: Yup.date().min(today, "Deal must not end today"),
-        // })}
+        validationSchema={Yup.object({
+          img: Yup.mixed().required("File is required"),
+          title: Yup.string()
+            .max(20, "Must be 30 characters or less")
+            .min(8, "Must be 8 characters and more")
+            .required("Title is required"),
+          category: Yup.string().required("Category is required"),
+          description: Yup.string().required("Description is required"),
+          vendor: Yup.string().required("Vendor is required"),
+          onlineAndOrStore: Yup.array()
+            .min(1)
+            .of(Yup.string().required("Select at least one"))
+            .required("Select at least one"),
+          url: Yup.string().required("Link is required").url("Link is invalid"),
+          dealsCategory: Yup.string().required("Please select one"),
+          // priceBeforeDiscount: Yup.number().required("Price is required"),
+          // priceAfterDiscount: Yup.number().required("Price is required"),
+          // custom: Yup.string().required("Field is required"),
+          endDate: Yup.date().min(
+            today,
+            "Deal must be valid for more than a day"
+          ),
+        })}
       >
         {({
           values,
@@ -54,40 +82,29 @@ function Submission() {
           <form onSubmit={handleSubmit}>
             <fieldset>
               <legend>Submission</legend>
-              <input
-                type="file"
-                name="img"
-                onChange={handleChange}
-                onBlur={handleBlur}
-                value={values.img}
-              />
+              <Field type="file" name="img" value={values.img} />
               {errors.img && touched.img && errors.img}
               <br />
               <label>
-                Title
-                <input
-                  name="title"
-                  onChange={handleChange}
-                  onBlur={handleBlur}
-                  value={values.title}
-                />
+                Title *
+                <Field name="title" value={values.title} />
                 {errors.title && touched.title && errors.title}
               </label>
               <label>Select category</label>
-
-              <select name="category">
+              <Field as="select" name="category">
                 <option value="">--Please choose an option--</option>
-                <option value="dog">Dog</option>
-                <option value="cat">Cat</option>
-                <option value="hamster">Hamster</option>
-                <option value="parrot">Parrot</option>
-                <option value="spider">Spider</option>
-                <option value="goldfish">Goldfish</option>
-              </select>
+                {categories?.data?.map((category) => (
+                  <option key={category?._id} value={category?._id}>
+                    {category?.classification}
+                  </option>
+                ))}
+                {errors.category && touched.category && errors.category}
+                {/* {Unable to get error message to work} */}
+              </Field>
 
               <br />
               <label>
-                Description
+                Description *
                 <textarea
                   name="description"
                   rows="6"
@@ -102,125 +119,98 @@ function Submission() {
               </label>
               <br />
               <label>
-                Vendor
-                <input
-                  name="vendor"
-                  onChange={handleChange}
-                  onBlur={handleBlur}
-                  value={values.vendor}
-                />
+                Vendor *
+                <Field name="vendor" value={values.vendor} />
                 {errors.vendor && touched.vendor && errors.vendor}
               </label>
               <br />
-              {/* <Field as="select" name="onlineAndOrStore">
-                <option
-                  name=""
-                  onChange={handleChange}
-                  onBlur={handleBlur}
-                  value="inStore"
-                >
-                  --Please choose an option--
-                </option>
-                <option
-                  name="onlineAndOrStore"
-                  onChange={handleChange}
-                  onBlur={handleBlur}
-                  value="inStore"
-                >
+              <div role="group" aria-labelledby="checkbox-group">
+                <label>
+                  <Field
+                    type="checkbox"
+                    name="onlineAndOrStore"
+                    value="inStore"
+                  />
                   In-Store
-                </option>
-                <option
-                  name="onlineAndOrStore"
-                  onChange={handleChange}
-                  onBlur={handleBlur}
-                  value="online"
-                >
+                </label>
+                <label>
+                  <Field
+                    type="checkbox"
+                    name="onlineAndOrStore"
+                    value="online"
+                  />
                   Online
-                </option>
-                <option
-                  name="onlineAndOrStore"
-                  onChange={handleChange}
-                  onBlur={handleBlur}
-                  value="both"
-                >
-                  Both
-                </option>
-              </Field> */}
-              {/* <label>
-                In-Store
-                <input
-                  type="checkbox"
-                  name="onlineAndOrStore"
-                  onChange={handleChange}
-                  onBlur={handleBlur}
-                  value={values.onlineAndOrStore}
-                />
+                </label>
                 {errors.onlineAndOrStore &&
                   touched.onlineAndOrStore &&
                   errors.onlineAndOrStore}
-              </label>
-              <label>
-                Online
-                <input
-                  type="checkbox"
-                  name="onlineAndOrStore"
-                  onChange={handleChange}
-                  onBlur={handleBlur}
-                  value={values.onlineAndOrStore}
-                />
-                {errors.onlineAndOrStore &&
-                  touched.onlineAndOrStore &&
-                  errors.onlineAndOrStore}
-              </label> */}
+              </div>
               <br />
               <label>
-                Link to source
-                <input
-                  name="url"
-                  onChange={handleChange}
-                  onBlur={handleBlur}
-                  value={values.url}
-                />
+                Link to source *
+                <Field name="url" value={values.url} />
                 {errors.url && touched.url && errors.url}
               </label>
               <br />
               <label>
-                {" "}
                 Address
-                <input
-                  name="address"
-                  onChange={handleChange}
-                  onBlur={handleBlur}
-                  value={values.address}
-                />
+                <Field name="address" value={values.address} />
                 {errors.address && touched.address && errors.address}
               </label>
-              <label>
-                Free
-                <input type="radio" name="dealscategory" value="free" />
-              </label>
-              <label>
-                Price Discounts
-                <input type="radio" name="dealscategory" value="discounts" />
-              </label>
-              <label>
-                Custom
-                <input type="radio" name="dealscategory" value="custom" />
-              </label>
-              <br />
+
+              <div role="group" aria-labelledby="my-radio-group">
+                <label>
+                  <Field type="radio" name="dealsCategory" value="free" />
+                  Free
+                </label>
+                <label>
+                  <Field type="radio" name="dealsCategory" value="discounts" />
+                  Price Discounts
+                </label>
+                <label>
+                  <Field type="radio" name="dealsCategory" value="custom" />
+                  Custom
+                </label>
+              </div>
+              {values.dealsCategory === "discounts" && (
+                <>
+                  <label>
+                    Price Before Discount
+                    <Field
+                      name="priceBeforeDiscount"
+                      value={values.priceBeforeDiscount}
+                    />
+                  </label>
+                  <label>
+                    <br />
+                    Price After Discount
+                    <Field
+                      name="priceAfterDiscount"
+                      value={values.priceAfterDiscount}
+                    />
+                  </label>
+                  <br />
+                </>
+              )}
+              {values.dealsCategory === "custom" && (
+                <>
+                  <label>
+                    <br />
+                    Custom
+                    <Field name="custom" value={values.custom} />
+                  </label>
+                  <br />
+                </>
+              )}
+
               <label>
                 Start Date
-                <input type="date" name="startDate" />
+                <Field type="date" name="startDate" value={values.startDate} />
+                {errors.startDate && touched.startDate && errors.startDate}
               </label>
               <label>
                 End Date
-                <input
-                  type="date"
-                  name="endDate"
-                  onChange={handleChange}
-                  onBlur={handleBlur}
-                  value={values.endDate}
-                />
+                <Field type="date" name="endDate" value={values.endDate} />
                 {errors.endDate && touched.endDate && errors.endDate}
               </label>
             </fieldset>
