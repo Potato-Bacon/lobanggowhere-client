9d4e517a0b7c25a56e45210b47745be66ac81617
junjiecheo12 <wkcheojj@gmail.com>
2022-09-18 22:17:49 +0800

    add route for navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,35 @@
+import { useNavigate } from "react-router-dom";
+import { useContext } from "react";
+import AuthContext from "../context/AuthProvider";
 const Navbar = () => {
+  const { auth } = useContext(AuthContext);
+  const navigate = useNavigate();
+  const handleNavigate = (e) => {
+    const text = e.target.innerText;
+    if (text === "Home" || text === "Logout") {
+      return navigate("/");
+    }
+    if (text === "Admin") {
+      return navigate("/admin");
+    }
+    if (text === "Profile") {
+      return navigate("/login");
+    }
+    if (text === "Login") {
+      return navigate("/login");
+    }
+  };
   return (
-    <div style={{ backgroundColor: "red" }}>
-      <h1>Lobang go where</h1>
+    <div style={{ backgroundColor: "red", display: "flex" }}>
+      <h3>Lobang go where</h3>
+      <button onClick={handleNavigate}>Home</button>
+      <button onClick={handleNavigate}>Admin</button>
+      <button onClick={handleNavigate}>Profile</button>
+      {auth ? (
+        <button onClick={handleNavigate}>Logout</button>
+      ) : (
+        <button onClick={handleNavigate}>Login</button>
+      )}
     </div>
   );
 };
