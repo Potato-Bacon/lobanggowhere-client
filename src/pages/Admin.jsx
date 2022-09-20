c433695679d5cd218ca4a37878f0516759397472
Ernest-Low <ernestlow32@gmail.com>
2022-09-20 19:59:05 +0800

    emptied out admin

--- src/pages/Admin.jsx
+++ src/pages/Admin.jsx
@@ -1,38 +1,7 @@
-import { useEffect } from "react";
-// import useVerify from "../hooks/useVerify";
-import useAxiosPrivate from "../hooks/useAxiosPrivate";
-import { useNavigate, useLocation } from "react-router-dom";
 
 function Admin() {
-  useEffect(() => {
-    const axiosPrivate = useAxiosPrivate();
-    const navigate = useNavigate();
-    const location = useLocation();
-
-    let isMounted = true;
-    const controller = new AbortController();
-
-    const getUsers = async () => {
-      try {
-        const response = await axiosPrivate.get("admin", {
-          signal: controller.signal,
-        });
-        console.log(response.data);
-        isMounted && setUsers(response.data);
-      } catch (err) {
-        console.error(err);
-        navigate("/login", { state: { from: location }, replace: true });
-      }
-    };
-
-    getUsers();
-
-    return () => {
-      isMounted = false;
-      controller.abort();
-    };
-  }, []);
-
+  
+    
   return (
     <>
       <h1>Admin</h1>
