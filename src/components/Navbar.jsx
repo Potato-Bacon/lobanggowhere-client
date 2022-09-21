fb48d2d52e56a0a8857c3f4f9b880515d0b61054
potatobacon <andylohbiz@gmail.com>
2022-09-21 23:47:01 +0800

    styling for homepage navbar done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -24,17 +24,54 @@ const Navbar = () => {
     }
   };
   return (
-    <div style={{ backgroundColor: "red", display: "flex" }}>
-      <h3>Lobang go where</h3>
-      <button onClick={handleNavigate}>Home</button>
-      <button onClick={handleNavigate}>Admin</button>
-      <button onClick={handleNavigate}>Profile</button>
-      {user.userName ? (
-        <button onClick={handleNavigate}>Logout</button>
-      ) : (
-        <button onClick={handleNavigate}>Login</button>
-      )}
-    </div>
+    <>
+      {/* <div style={{ backgroundColor: "red", display: "flex" }}>
+        <h3>Lobang go where</h3>
+        <button onClick={handleNavigate}>Home</button>
+        <button onClick={handleNavigate}>Admin</button>
+        <button onClick={handleNavigate}>Profile</button>
+        {user.userName ? (
+          <button onClick={handleNavigate}>Logout</button>
+        ) : (
+          <button onClick={handleNavigate}>Login</button>
+        )}
+      </div> */}
+      <div className="navbar bg-base-100">
+        <div className="flex-1">
+          <a className="btn btn-ghost normal-case text-xl">LobangGoWhere</a>
+        </div>
+        <div className="flex-none gap-2">
+          <div className="dropdown dropdown-end">
+            <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
+              <div className="w-10 rounded-full">
+                <img src="https://placeimg.com/80/80/people" />
+              </div>
+            </label>
+            <ul
+              tabIndex={0}
+              className="mt-3 p-2 shadow menu menu-compact dropdown-content bg-base-100 rounded-box w-52"
+            >
+              <li>
+                <a onClick={handleNavigate} className="justify-between">
+                  Profile
+                  <span className="badge">New</span>
+                </a>
+              </li>
+              <li>
+                <a onClick={handleNavigate}>Admin</a>
+              </li>
+              <li>
+                {user.userName ? (
+                  <a onClick={handleNavigate}>Logout</a>
+                ) : (
+                  <a onClick={handleNavigate}>Login</a>
+                )}
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 export default Navbar;
