942c8eee7f094bfcb76c4217e4d9a97e4b47a87e
junjiecheo12 <wkcheojj@gmail.com>
2022-09-22 13:18:21 +0800

    complete register and login css

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -72,61 +72,81 @@ const Login = () => {
   };
 
   return (
-    <div className="text-center">
-      <div className="flex items-center card w-96 h-100 bg-base-100 shadow-xl text-center ">
-        <p
-          ref={errRef}
-          className={errMsg ? "errmsg" : "offscreen"}
-          aria-live="assertive"
-        >
-          {errMsg}
-        </p>
-        <form onSubmit={handleSubmit} className={"mt-11"}>
-          <div className="flex items-center justify-center">
-            <label htmlFor="userName" className="card-body text-xl m-0 pr-2">
-              Username:{" "}
-            </label>
-            <input
-              type="text"
-              id="userName"
-              ref={userRef}
-              autoComplete="off"
-              onChange={(e) => setUserName(e.target.value)}
-              value={userName}
-              required
-              placeholder="Enter username ..."
-              className="input input-bordered input-primary w-full max-w-xs"
-            />
-          </div>
-          <div className="flex items-center justify-center">
-            <label htmlFor="password" className="card-body text-xl m-0 pr-4">
-              Password:{" "}
-            </label>
-            <input
-              type="password"
-              id="password"
-              onChange={(e) => setPassword(e.target.value)}
-              value={password}
-              required
-              placeholder="Enter password ..."
-              className="input input-bordered input-primary w-full max-w-xs"
-            />
-          </div>
-          <button className="btn btn-primary mt-1.5">Sign In</button>
-        </form>
-        <p className="card-body pb-0.5">
-          Need an Account?
-          <br />
-          <span
-            onClick={() => {
-              console.log("Navigating to register");
-              navigate("/register");
-            }}
-            className="card-body underline pt-1.5 mb-3.5"
+    <div className="flex-col flex items-center justify-center px-8 py-8 sm:px-12 lg:col-span-7 lg:py-12 lg:px-16 xl:col-span-6">
+      <h1 className="mt-6 text-2xl font-bold text-gray-900 sm:text-3xl md:text-4xl">
+        Welcome to LobangGoWhere 💸
+      </h1>
+
+      <p className="mt-4 leading-relaxed text-gray-500 w-96">
+        Ut eros ante, blandit vel tincidunt at, tristique ut turpis. Donec elit
+        ligula, suscipit id magna sed, fermentum dictum ex
+      </p>
+      <div className="flex justify-center ">
+        <div className="flex items-center card w-96 h-100 bg-base-100 text-center ">
+          <p
+            ref={errRef}
+            className={errMsg ? "errmsg" : "offscreen"}
+            aria-live="assertive"
           >
-            Sign Up
-          </span>
-        </p>
+            {errMsg}
+          </p>
+          <form onSubmit={handleSubmit} className={"mt-8"}>
+            <div className="flex items-center justify-center">
+              <label
+                htmlFor="userName"
+                className="block text-lg font-medium text-gray-700 pr-1"
+              >
+                Username:{" "}
+              </label>
+              <input
+                type="text"
+                id="userName"
+                ref={userRef}
+                autoComplete="off"
+                onChange={(e) => setUserName(e.target.value)}
+                value={userName}
+                required
+                placeholder="Enter username ..."
+                className="pl-2 w-full mt-1 h-8 text-base text-gray-700 bg-white border-gray-200 rounded-md shadow-sm"
+              />
+            </div>
+            <div className="flex items-center justify-center py-4">
+              <label
+                htmlFor="password"
+                className="block text-lg font-medium text-gray-700 pr-1"
+              >
+                Password:{" "}
+              </label>
+              <input
+                type="password"
+                id="password"
+                onChange={(e) => setPassword(e.target.value)}
+                value={password}
+                required
+                placeholder="Enter password ..."
+                className="pl-2 w-full mt-1 h-8 text-base text-gray-700 bg-white border-gray-200 rounded-md shadow-sm"
+              />
+            </div>
+            <div className="col-span-6 sm:flex sm:items-center sm:gap-4 mt-3">
+              <button className="inline-block px-12 py-3 text-sm font-medium text-white bg-primary border border-primary transition rounded-md shrink-0 hover:bg-transparent hover:text-primary focus:outline-none focus:ring active:text-primary">
+                Sign In
+              </button>
+
+              <p className="mt-4 text-sm text-gray-500 sm:mt-0">
+                Need an Account?
+              </p>
+              <p
+                onClick={() => {
+                  console.log("Navigating to register");
+                  navigate("/register");
+                }}
+                className="text-gray-700 underline "
+              >
+                Sign Up
+              </p>
+            </div>
+          </form>
+        </div>
       </div>
     </div>
   );
